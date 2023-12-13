fda1026ccc4f227a87b5899508afd38638a81b2e
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-12-13 20:10:12 +0100

    compras ok

--- app/Http/Controllers/CompraController.php
+++ app/Http/Controllers/CompraController.php
@@ -1,8 +1,18 @@
 <?php
 
+
+
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
+use App\Models\Compra;
+use App\Models\Producto;
+use Exception;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
 
 class CompraController extends Controller
 {
@@ -37,10 +47,55 @@ class CompraController extends Controller
                 return ApiResponse::error('No se permiten productos duplicados para la compra', 400);
             }
 
-    
+            $totalPagar = 0;
+            $subtotal = 0;
+            $compraItems = [];
+
+            // Iteración de los productos para calcular el total a pagar
+            foreach ($productos as $producto) {
+                $productoB = Producto::find($producto['producto_id']);
+                if (!$productoB) {
+                    return ApiResponse::error('Producto no encontrado', 404);
+                }
+
+                // Validar la cantidad disponible de los productos
+                if ($productoB->cantidad_disponible < $producto['cantidad']) {
+                    return ApiResponse::error('El producto no tiene suficiente cantidad disponible', 404);
+                }
+
+                // Actualización de la cantidad disponible de cada producto
+                $productoB->cantidad_disponible -= $producto['cantidad'];
+                $productoB->save();
+
+                // Cálculo de los importes
+                $subtotal = $productoB->precio * $producto['cantidad'];
+                $totalPagar += $subtotal;
+               
+                // Items de la compra
+                $compraItems[] = [
+                    'producto_id' => $productoB->id,
+                    'precio' => $productoB->precio,
+                    'cantidad' => $producto['cantidad'],
+                    'subtotal' => $subtotal
+                ];
+            }
+            // Registro en la tabla compra
+            $compra = Compra::create([
+                'subtotal' => $totalPagar,
+                'total' => $totalPagar
+            ]);
+            // Asociar los productos a la compra con sus cantidades y sus subtotales
+            $compra->productos()->attach($compraItems);
+
+            return ApiResponse::success('Compra realizada exitosamente', 201, $compra);
+   
+        } catch (QueryException $e) {
+            // error de consulta en la base de datos
+            return ApiResponse::error('Error en la consulta de base de datos', 500);
         } catch (Exception $e) {
-            // Aquí iría el manejo de la excepción
+            return ApiResponse::error('Error inesperado', 500);
         }
+        
     }
     
 
